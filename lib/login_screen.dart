e67d538913c120a53a9874855d3baa7400cc381f
anson <ansonlcy1214@gmail.com>
2025-04-27 02:17:59 +0800

    login screen ui change

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -58,7 +58,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
+      appBar: AppBar(title: const Text('')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
@@ -66,15 +66,37 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                const Text(
+                  'Welcome back!\n\nPlease Login.',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                // Input fields for email and password
                 TextField(
                   controller: _emailController,
-                  decoration: const InputDecoration(labelText: 'Email'),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.email),
+                    labelText: 'Email',
+                    helperText: 'Enter your registered email address',
+                    helperStyle: TextStyle(fontSize: 12),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                    ),
                   keyboardType: TextInputType.emailAddress,
+                  
                 ),
-                const SizedBox(height: 10),
+                const SizedBox(height: 30),
                 TextField(
                   controller: _passwordController,
-                  decoration: const InputDecoration(labelText: 'Password'),
+                  decoration: const InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    helperText: 'Enter your password',
+                    helperStyle: TextStyle(fontSize: 12),
+                    labelText: 'Password',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                    ),
                   obscureText: true,
                 ),
                 const SizedBox(height: 20),
@@ -82,18 +104,41 @@ class _LoginScreenState extends State<LoginScreen> {
                   Padding(
                     padding: const EdgeInsets.only(bottom: 10.0),
                     child: Text(
-                      _errorMessage,
+                      "Please check your email and password",
                       style: const TextStyle(color: Colors.red),
                       textAlign: TextAlign.center,
                     ),
                   ),
+                const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: _signIn,
                   child: const Text('Login'),
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 20),
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.lightGreen,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 15,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: const BorderSide(color: Colors.lightGreen),
+                    ),
+                  ),
                 ),
+                const SizedBox(height: 10),
                 TextButton(
                   onPressed: widget.showRegisterScreen, // Use the callback
                   child: const Text('Don\'t have an account? Register'),
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 16),
+                    foregroundColor: Colors.blue,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 15,
+                    ),
+                  ),
                 ),
               ],
             ),
